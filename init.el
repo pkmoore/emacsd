df81849422a5f72707ad6e8ab5bf85cd9f6047a2
Preston Moore <prestonkmoore@gmail.com>
2016-05-22 22:07:09 +0000

    Updated

--- init.el
+++ init.el
@@ -26,15 +26,13 @@
 ;; enable and configure helm
 (require 'helm-config)
 (helm-mode 1)
+(setq helm-mode-fuzzy-match t)
+(setq helm-completion-in-region-fuzzy-match t)
 (global-set-key (kbd "M-x") 'helm-M-x)
 
 ;; enable line numbers all the time
-(global-nlinum-mode t)
-(setq-default nlinum-format "%d ")
-
-;;
-(global-diff-hl-mode t)
-(diff-hl-margin-mode t)
+;;(global-nlinum-mode t)
+;;(setq-default nlinum-format "%d ")
 
 ;; enable and configure projectile-related stuff
 (projectile-global-mode)
@@ -48,7 +46,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("4ab86c7682db09485a6e046ee0a6c45b5462f514c89844f4ed8b329aa1708067" "37def0fac11a4890922af9febc8394e3b6e3c68904a294a2d440b1904e979c7e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+    ("82b67c7e21c3b12be7b569af7c84ec0fb2d62105629a173e2479e1053cff94bd" "4ab86c7682db09485a6e046ee0a6c45b5462f514c89844f4ed8b329aa1708067" "37def0fac11a4890922af9febc8394e3b6e3c68904a294a2d440b1904e979c7e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -98,7 +97,8 @@
 
 (defun my-python-hook ()
   (fci-mode)
-  (setq fill-column 80))
+  (setq fill-column 80)
+  (flycheck-mode))
 (add-hook 'python-mode-hook 'my-python-hook)
 
 ;; magit-status keybind
