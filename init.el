40d5499d4ec8a5bfbcfeb763ee4919e85618f997
Preston Moore <prestonkmoore@gmail.com>
2017-01-09 01:26:48 -0500

    neotree keybind

--- init.el
+++ init.el
@@ -126,3 +126,5 @@
     (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
     (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
     (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+(global-set-key (kbd "C-c t") 'neotree-toggle)
+
