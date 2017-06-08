645c8986acc48e98fd10bc789548c51a368c37fe
Preston Moore <prestonkmoore@gmail.com>
2017-06-08 14:43:39 -0400

    line hilight, better line number

--- init.el
+++ init.el
@@ -35,8 +35,40 @@
 ;; enable line numbers all the time
 ;;(global-nlinum-mode t)
 ;;(setq-default nlinum-format "%d ")
-(linum-relative-global-mode)
-(helm-linum-relative-mode)
+;;(linum-relative-global-mode)
+;;(helm-linum-relative-mode)
+
+(require 'hl-line)
+
+(defface my-linum-hl
+  `((t :inherit linum :background ,(face-background 'hl-line nil t)))
+  "Face for the current line number."
+  :group 'linum)
+
+(add-hook 'linum-before-numbering-hook 'my-linum-get-format-string)
+
+(defun my-linum-get-format-string ()
+  (let* ((width (1+ (length (number-to-string
+                             (count-lines (point-min) (point-max))))))
+         (format (concat "%" (number-to-string width) "d \u2502")))
+    (setq my-linum-format-string format)))
+
+(defvar my-linum-current-line-number 0)
+
+(setq linum-format 'my-linum-format)
+
+(defun my-linum-format (line-number)
+  (propertize (format my-linum-format-string line-number) 'face
+              (if (eq line-number my-linum-current-line-number)
+                  'my-linum-hl
+                'linum)))
+
+(defadvice linum-update (around my-linum-update)
+  (let ((my-linum-current-line-number (line-number-at-pos)))
+    ad-do-it))
+(ad-activate 'linum-update)
+(global-linum-mode t)
+(global-hl-line-mode t)
 
 ;; enable and configure projectile-related stuff
 (projectile-global-mode)
